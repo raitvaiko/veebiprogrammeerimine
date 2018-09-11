8f11a0e6f62feb1f47c512bd75cea5d0629c733b
Rait Väiko <raitvaiko@users.noreply.github.com>
2018-09-11 14:22:28 +0300

    lisatud esimene fail

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"> 
+	<title>Rait Väiko, õppetöö</title>
+	
+  </head>
+  <body>
+    <h1>Rait Väiko</h1>
+	<h2>...</h2>
+    <p>TLÜ koduleht:  <a href="http://www.tlu.ee">TLÜ</a>
+	
+	<p>Minu sõber teeb ka veebi <a href "../../../~rasmaro" target ="_blank">LINK</a> </p>
+	
+	<p>Tere. Ei oskagi midagi kirjutada siia.</p>
+
+	<img src="../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_2.jpg" alt="Tlü Terra õppehoone"
+	
+	
+
+ </body>
+</html>
